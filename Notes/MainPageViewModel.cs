c9fd174f8fcd503c2f27cda1a84db2c8f5adf7c3
Paul Wenzel <paul.wenzel@netlight.com>
2020-11-02 19:15:20 +0100

    Basic layout of MainPage

---
+++ Notes/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Notes
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public List<Note> Notes { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            Notes = new List<Note>();
+            Notes.Add(new Note("Test"));
+            Notes.Add(new Note("Test1"));
+            Notes.Add(new Note("Test2"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+            Notes.Add(new Note("Test3"));
+
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Notes/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Notes
+{
+    public class Note
+    {
+
+        public string Content { get; set; }
+
+        public Note(string content)
+        {
+            Content = content;
+        }
+    }
+}
