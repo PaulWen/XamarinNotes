cac3502b890104a67538bf12492134dc22585c09
Paul Wenzel <paul.wenzel@netlight.com>
2020-11-02 20:51:09 +0100

    Implement Add button command and disable button as long as the new note is empty

--- Notes/MainPageViewModel.cs
+++ Notes/MainPageViewModel.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Notes
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
-        public List<Note> Notes { get; }
+        private string _newNotes;
+        public string NewNote
+        {
+            get { return _newNotes; }
+            set
+            {
+                _newNotes = value;
+                AddNote.ChangeCanExecute();
+            }
+        }
+
+        public ObservableCollection<Note> Notes { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public Command AddNote { protected set; get; }
+        public Command DiscardNote { protected set; get; }
+
+
         public MainPageViewModel()
         {
-            Notes = new List<Note>();
+            AddNote = new Command(AddNewNote, CanAddNewNote);
+
+            NewNote = "";
+            Notes = new ObservableCollection<Note>();
+
+
             Notes.Add(new Note("Test"));
             Notes.Add(new Note("Test1"));
             Notes.Add(new Note("Test2"));
@@ -26,5 +49,17 @@ namespace Notes
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+
+        private void AddNewNote()
+        {
+            Notes.Add(new Note(NewNote.Trim()));
+            NewNote = "";
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NewNote"));
+        }
+
+        private bool CanAddNewNote()
+        {
+            return !string.IsNullOrWhiteSpace(NewNote);
+        }
     }
 }
